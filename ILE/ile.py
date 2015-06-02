3a169a377e8402ce4714cda43d63ecb73fd8977b
tanweijun <a0125360@u.nus.edu>
2015-06-02 17:55:20 +0800

    First GAE usage
    Testing and learning GAE. Tried to run app to show homepage but failed.

---
+++ ILE/ile.py
@@ -0,0 +1,21 @@
+#import urllib
+import webapp2
+import jinja2
+import os
+#import datetime
+
+#from google.appengine.ext import ndb
+#from google.appengine.api import users
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
+
+class HomePage(webapp2.RequestHandler):
+    # Handler for the home page.
+    def get(self):
+        template = jinja_environment.get_template('home.html')
+        self.response.out.write(template.render())
+
+app = webapp2.WSGIApplication([
+    ('/', HomePage)], 
+	debug=True)
